b2aa517a3a02cd71cf35dd89db6afd8cffb62cac
Jesus Calamani <jesuscalamani92@gmail.com>
2023-07-02 00:59:15 -0400

    message

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,20 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+      colors: {
+				primary: {
+					50: '#eff6ff',
+					100: '#dbeafe',
+					200: '#bfdbfe',
+					300: '#93c5fd',
+					400: '#60a5fa',
+					500: '#3b82f6',
+					600: '#2563eb',
+					700: '#1d4ed8',
+					800: '#1e40af',
+					900: '#1e3a8a',
+				},
+			},
       textColor: {
         cris: {
           inverted: withOpacity('--fill-color'),
@@ -82,5 +96,8 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('flowbite/plugin'),
+		require('flowbite-typography'),
+		require('tailwind-scrollbar')({ nocompatible: true }),
 	],
 }
